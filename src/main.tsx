58230198b46a5a81b214077db6e74a9adb56291e
Aman Mavai <amanmavai@gmail.com>
2023-04-07 16:26:11 +0000

    apply prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,48 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ContactsRoot, { loader as contactsRootLoader, action as contactsRootAction } from "./routes/contacts_root";
+import Root from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact, {loader as contactLoader, action as contactAction} from "./routes/contact";
-import EditContact, {action as editAction} from "./routes/edit";
+import Contact, { loader as contactLoader, action as contactAction } from "./routes/contact";
+import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
 import Index from "./routes/index";
-import "./tailwind.css";
-import './index.css';
+import "./styles/tailwind.css";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
     children: [
-      { index: true, element: <Index /> },
       {
-        path: "/contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-        action: contactAction,
-        errorElement: <ErrorPage />
+        path: "cx",
+        element: <ContactsRoot />,
+        errorElement: <ErrorPage />,
+        loader: contactsRootLoader,
+        action: contactsRootAction,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+            errorElement: <ErrorPage />,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ],
       },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: destroyAction,
-        errorElement: <div>Oops! There was an error.</div>,
-      },
-    ]
-  }
-])
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
