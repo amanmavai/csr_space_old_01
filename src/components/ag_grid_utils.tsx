4e0335be31c52b0f182b4d57ef7fafa5dfc154eb
Aman Mavai <amanmavai@gmail.com>
2023-07-02 11:10:28 +0000

    add new statusBar component

---
+++ src/components/ag_grid_utils.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { getTotalRowCount, getFilteredRowCountValue } from "./ag_grid_table";
+import { LabelValue } from "./components";
+
+export const CustomTotalAndFilteredRowCount = (props) => {
+  const [totalCount, setTotalCount] = React.useState(0);
+  const [filteredCount, setFilteredCount] = React.useState(0);
+
+  React.useEffect(() => {
+    props.api.addEventListener("modelUpdated", updateCounts);
+    updateCounts();
+
+    return () => {
+      props.api.removeEventListener("modelUpdated", updateCounts);
+    };
+  }, [props.api]);
+
+  function updateCounts() {
+    setTotalCount(getTotalRowCount(props.api));
+    setFilteredCount(getFilteredRowCountValue(props.api));
+  }
+
+  let value;
+  if (filteredCount === totalCount) {
+    value = `${totalCount}`;
+  } else {
+    value = `${filteredCount} of ${totalCount}`;
+  }
+
+  return <LabelValue label={props.label || "Rows"} value={value} className={"tw-py-2"} />;
+};
